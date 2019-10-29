8ba48dc565ed64d82dfd19c8c9bc1562c1a44a33
Babar <babar.waheed@gmail.com>
2019-10-29 21:01:46 +1100

    https://github.com/babel/babel-loader
    npm install -D babel-loader @babel/core @babel/preset-env webpack
    
    Version update

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,20 @@ const config = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 };
 
