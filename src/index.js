a430551553dec4ac72dc211a7ab0a40e8e94c7b3
Babar <babar.waheed@gmail.com>
2019-10-29 19:59:04 +1100

    linking files with CommonJS

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//each file has its own scope
+
+//CommonJS = implementation by Node.js
+    //require / module.exports
+//AMD asynchronous module definition
+    //require, define
+//ES2015
+    // import, export
+//CommonJS
+const sum = require("./sum");
+
+console.log(sum(1, 2));
